ac9472226177d40f6d8eff37269843f7ab7f80c5
Elamparuthi Karunakaran <elamparuthik1991@gmail.com>
2021-02-25 23:15:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,10 +134,17 @@ def create():
         Ensure you pass a custom ID as part of json body in post request,
         e.g. json={'id': '1', 'title': 'Write a blog post'}
     """
-    
-        id = request.json['id']
-        todo_ref.document(id).set(request.json)
-        return jsonify({"success": True}), 200
+    try:
+        # Check if ID was passed to URL query
+        todo_id = request.args.get('id')
+        if todo_id:
+            todo = todo_ref.document(todo_id).get()
+            return jsonify(todo.to_dict()), 200
+        else:
+            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
+            return jsonify(all_todos), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
     
 
 @app.route('/list', methods=['GET'])
